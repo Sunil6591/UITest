2af8ef1226c8197fc8b885f84d406c37b1c63f21
Sunil Joshi <suniljoshi.is.available@gmail.com>
2014-06-01 14:02:39 +0530

    Added angular route

--- App/app/js/app.js
+++ App/app/js/app.js
@@ -1,4 +1,20 @@
 'use strict';
 
 
-var myFirstApp = angular.module('myFirstApp',['ngResource','ngRoute','ngSanitize','kendo.directives','ui.bootstrap']);
+var myFirstApp = angular.module('myFirstApp',['ngResource','ngRoute','ngSanitize','kendo.directives','ui.bootstrap'])
+.config(function($routeProvider) {
+	$routeProvider
+	.when("/home",
+	{
+		templateUrl: "templates/main.html"
+	})
+	.when("/employeeDir",
+	{
+		templateUrl: "templates/employeeDir.html"
+	})
+	.when("/search",
+	{
+		templateUrl: "templates/search.html"
+	})
+	.otherwise({redirectTo:"/home"});
+});
--- App/app/js/controllers/MainController.js
+++ App/app/js/controllers/MainController.js
@@ -2,14 +2,6 @@ myFirstApp.controller("MainController",function($scope,mainData) {
 
 	mainData.get(function(data) {
 		$scope.main = data;
-
-		$scope.main.tabUrls = new kendo.data.ObservableArray([]);
-
-		_.each($scope.main.tabs, function(tab) { 
-
-			$scope.main.tabUrls.push(tab.url);
-
-		});
 	});
 
 });
