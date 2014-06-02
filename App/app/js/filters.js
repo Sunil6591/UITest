57a9a78c75a3727e5276e51f0932d6ff2f334c96
Sunil <Sunil.P.Joshi@ap-pun-ws0414.internal.sungard.corp>
2014-06-02 12:26:35 +0530

    Restructuring

--- App/app/js/filters.js
+++ App/app/js/filters.js
@@ -1,12 +1,1 @@
-'use strict';
 
-myFirstApp.filter("durations", function() {
-	return function(duration) {
-		switch(duration) {
-			case 1: return "Half hour";
-			case 2: return "Two hours";
-			case 4: return "Half day";
-			case 8: return "Full day";
-		}
-	}
-})
